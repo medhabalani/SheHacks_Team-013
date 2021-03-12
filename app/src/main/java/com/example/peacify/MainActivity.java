1b79f3c932b16084a09f1ae4707cd4d62076a5b9
vidushi1012 <iit2019027@iiita.ac.in>
2021-03-13 01:31:01 +0530

    Feat:splashScreen

---
+++ app/src/main/java/com/example/peacify/MainActivity.java
@@ -0,0 +1,66 @@
+package com.example.peacify;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.widget.Button;
+import android.widget.TextView;
+import android.view.animation.AnimationUtils;
+
+
+
+public class MainActivity extends AppCompatActivity {
+
+    View first, second, third, fourth, fifth, sixth;
+    TextView t, slogan;
+    Animation topAnimation, bottomAnimation, middleAnimation;
+    Button goAhead;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+
+        topAnimation = AnimationUtils.loadAnimation(this, R.anim.top_animation);
+        bottomAnimation = AnimationUtils.loadAnimation(this, R.anim.bottom_animation);
+        middleAnimation = AnimationUtils.loadAnimation(this, R.anim.middle_animation);
+
+        first = findViewById(R.id.first_line);
+        second = findViewById(R.id.second_line);
+        third = findViewById(R.id.third_line);
+        fourth = findViewById(R.id.fourth_line);
+        fifth = findViewById(R.id.fifth_line);
+        sixth = findViewById(R.id.sixth_line);
+
+        t = findViewById(R.id.title);
+        slogan = findViewById(R.id.tag);
+
+        first.setAnimation(topAnimation);
+        second.setAnimation(topAnimation);
+        third.setAnimation(topAnimation);
+        fourth.setAnimation(topAnimation);
+        fifth.setAnimation(topAnimation);
+        sixth.setAnimation(topAnimation);
+
+        t.setAnimation(middleAnimation);
+        slogan.setAnimation(bottomAnimation);
+
+        int SPLASH_TIME_OUT = 5000;
+        new Handler().postDelayed(new Runnable(){
+            public void run(){
+                // check if the user is already logged in
+                // then go directly to dashboard
+                // else go to login page
+                Intent intent = new Intent(MainActivity.this, login_register.class);
+                startActivity(intent);
+            }
+        }, SPLASH_TIME_OUT);
+//
+    }
+}
---
+++ app/src/main/java/com/example/peacify/login_register.java
@@ -0,0 +1,15 @@
+package com.example.peacify;
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class login_register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_register);
+    }
+}
