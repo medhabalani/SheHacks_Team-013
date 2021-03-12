0b37ba1214629e7edd695dd21fb7ce2a5b2194e4
medhabalani <iit2019021@iiita.ac.in>
2021-03-13 03:01:44 +0530

    feat : firebase email authentication

--- app/src/main/java/com/example/peacify/Register.java
+++ app/src/main/java/com/example/peacify/Register.java
@@ -1,14 +1,80 @@
 package com.example.peacify;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Register extends AppCompatActivity {
-
+    EditText rfullname,remail,rpassword,rphone;
+    Button rregisterbtn;
+    FirebaseAuth fAuth;
+    ProgressBar progressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        EditText rfullname,remail,rpassword,rphone;
+        Button rregisterbtn;
+        FirebaseAuth fAuth;
+        ProgressBar progressBar;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        rfullname=findViewById(R.id.fullname);
+        remail=findViewById(R.id.email);
+        rpassword=findViewById(R.id.password);
+        rphone=findViewById(R.id.phone);
+        rregisterbtn=findViewById(R.id.registerbtn);
+        fAuth=FirebaseAuth.getInstance();
+        progressBar=findViewById(R.id.progressBar);
+
+        if(fAuth.getCurrentUser() != null){
+            startActivity(new Intent(getApplicationContext(),login.class));
+            finish();
+        }
+        rregisterbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email=remail.getText().toString().trim();
+                String pass=rpassword.getText().toString().trim();
+                if(TextUtils.isEmpty(email)){
+                    remail.setError("Email is required.");
+                    return;
+                }
+                if(TextUtils.isEmpty(pass)){
+                    rpassword.setError("Password is required.");
+                    return;
+                }
+                if(pass.length()<6){
+                    rpassword.setError("Password must be >= 6 character.");
+                }
+                progressBar.setVisibility(View.VISIBLE);
+                fAuth.createUserWithEmailAndPassword(email,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(Register.this, "Registered successfully", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(),login.class));
+                        }
+                        else{
+                            Toast.makeText(Register.this, "Error: "+task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            progressBar.setVisibility(View.INVISIBLE);
+                        }
+
+                    }
+                });
+            }
+        });
+
     }
 }
--- app/src/main/java/com/example/peacify/login.java
+++ app/src/main/java/com/example/peacify/login.java
@@ -1,14 +1,67 @@
 package com.example.peacify;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class login extends AppCompatActivity {
+    EditText lemail,lpassword;
+    Button lloginBtn;
+    ProgressBar progressBar;
+    FirebaseAuth fAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        lemail=findViewById(R.id.email2);
+        lpassword=findViewById(R.id.password2);
+        lloginBtn=findViewById(R.id.loginBtn);
+        progressBar=findViewById(R.id.progressBar2);
+        fAuth=FirebaseAuth.getInstance();
+
+        lloginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email=lemail.getText().toString().trim();
+                String pass=lpassword.getText().toString().trim();
+                if(TextUtils.isEmpty(email)){
+                    lemail.setError("Email is required.");
+                    return;
+                }
+                if(TextUtils.isEmpty(pass)){
+                    lpassword.setError("Password is required.");
+                    return;
+                }
+                if(pass.length()<6){
+                    lpassword.setError("Password must be >= 6 character.");
+                }
+                progressBar.setVisibility(View.VISIBLE);
+                fAuth.signInWithEmailAndPassword(email,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(login.this, "Logged in successfully", Toast.LENGTH_SHORT).show();
+                        }
+                        else{
+                            Toast.makeText(login.this, "Error: "+task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            progressBar.setVisibility(View.INVISIBLE);
+                        }
+                    }
+                });
+            }
+        });
     }
 }
